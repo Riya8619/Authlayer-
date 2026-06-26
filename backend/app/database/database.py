3e25023a947da9da60e3c166b938b5643958a3d2
Preya Patel <preyapatel0702@gmail.com>
2026-06-26 22:12:58 +0530

    Changes to PostgreSQL

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -1,12 +1,24 @@
+import os
+
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./authlayer.db"
+)
 
-DATABASE_URL = "sqlite:///./authlayer.db"
+engine_kwargs = {}
+
+# SQLite requires check_same_thread
+if DATABASE_URL.startswith("sqlite"):
+    engine_kwargs["connect_args"] = {
+        "check_same_thread": False
+    }
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    **engine_kwargs
 )
 
 SessionLocal = sessionmaker(
